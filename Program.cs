88bd036a2f491020a4aee3730860d3ce3d8fdff4
Vavadjan <vavadjan@gmail.com>
2023-09-28 00:31:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace thirdDegree
+{
+    internal class Program
+    {
+        static int ThirdDegree(int x)
+        {
+            int result = x * x * x;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа возведёт число в третюю степень.");
+            Console.WriteLine("Введите число: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = ThirdDegree(number);
+
+            Console.WriteLine("Число " + number + " в третей степени будет равно " + result);
+        }
+    }
+}
